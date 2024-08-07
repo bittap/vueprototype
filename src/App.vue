3110c197f7ed1308b4fd977b1ffd46aa411f7fc9
bittap <kinjwo1@naver.com>
2024-08-07 18:58:25 +0900

    データをexportし、vueファイルでimportし、画面に表示までコミット

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,72 @@
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
-  <div>
-    {{ logo }}
-    <h4 :style="style">{{ products[0] }}</h4>
-    <p>{{ price1 }}万円</p> 
+
+  <div class="black-bg" v-if="modal_opend == false">
+    <div class="white-bg">
+      <h4>詳細ページ</h4>
+      <p>ページ内容</p>
+    </div>
   </div>
+
   <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ price2 }}万円</p> 
+    <h4 @click="modal_opend = true">ワンルーム</h4>
+  </div>
+
+  <div v-for="(product, i) in products" :key="i">
+    <img :src="product.image" />
+    <h4>{{ product.title }}</h4>
+    <p>{{ product.price }}</p>
   </div>
 </template>
 
 <script>
+import data from "./assets/oneroom.js";
+
 export default {
   name: "App",
   data() {
     return {
+      modal_opend: true,
       logo: "ワンルームショップ",
       price1: 60,
       price2: 70,
       style: "color : blue",
-      products: ["aaa", "bbb", "ccc"],
+      products: data,
       menus: ["home", "product", "about"],
     };
+  },
+  methods: {
+    increase() {
+      this.count++;
+    },
   },
   components: {},
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
